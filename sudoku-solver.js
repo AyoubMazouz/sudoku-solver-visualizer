5fe25971df6e17498b0e20b1114519a01c7c25c1
= <=>
2021-12-16 03:40:46 +0100

    I need to sleep

--- sudoku-solver.js
+++ sudoku-solver.js
@@ -1,12 +1,13 @@
 const getValidPosition = board => {
     for (let y = 0; y < board.length; y++) {
         for (let x = 0; x < board[0].length; x++) {
-            if (board[y][x] === 0) return [y, x]
+            if (board[y][x] === 0) return new Vector2(y, x)
         }
     } return false
 }
 
-const isValidNumber = (y, x, board, number) => {
+const isValidNumber = (vector, board, number) => {
+    const [x, y] = [vector.x, vector.y]
     // Check in all columns except at y & x
     for (let i = 0; i < board[y].length - 1; i++) {
         if (board[i][x] === number && [y, x] !== [i, x]) {
@@ -33,14 +34,14 @@ const isValidNumber = (y, x, board, number) => {
 }
 
 const solve = board => {
-    const position = getValidPosition(board)
+    const pos = getValidPosition(board)
 
     if (!position) return true
 
-    let [y, x] = position
+    let [y, x] = [pos.x, pos.y]
 
     for (let n = 0; n < 10; n++) {
-        if (isValidNumber(y, x, board, n)) {
+        if (isValidNumber(pos, board, n)) {
 
             board[y][x] = n
 
@@ -51,6 +52,132 @@ const solve = board => {
     } return false
 }
 
-test(tests)
+class Vector2 {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+    getPos() {
+        return [this.x, this.y]
+    }
+    plus(vector) {
+        return new Vector2(this.x + vector.x, this.y + vector.y)
+    }
+}
 
+class Block {
+    constructor(pos, value, stat) {
+        this.pos = pos
+        this.value = value
+        this.stat = stat
+    }
+}
+
+const canvas = document.getElementById('canvas')
+/** @type {CanvasRenderingContext2D} */
+const ctx = canvas.getContext('2d')
+
+const cellSize = 50
+canvas.width = cellSize * 9
+canvas.height = cellSize * 9
+
+const solveBtn = document.getElementById('solve')
+
+const game = {
+    originalBoard: tests[0].board,
+    board: null,
+    cell: null,
+}
+
+const getIndex = pos => {
+    return new Vector2(pos.x / cellSize | 0, pos.y / cellSize | 0)
+}
+
+const getMousePos = event => {
+    return new Vector2(event.clientX, event.clientY)
+}
+
+const clearCanvas = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+const drawBoard = board => {
+    ctx.font = `${cellSize}px sans serif`
+    for (let y = 0; y < board.length; y++) {
+        for (let x = 0; x < board.length; x++) {
+            if (game.board[y][x].stat == 'error') {
+                ctx.fillStyle = '#943'
+            }
+            else if (game.board[y][x].stat == 'success') {
+                ctx.fillStyle = '#049'
+            }
+            ctx.fillText(game.board[y][x].value, x * cellSize, cellSize + y * cellSize)
+            ctx.fillStyle = '#000'
+        }
+    }
+}
+
+const draw = () => {
+    clearCanvas()
+    drawBoard(game.board)
+}
+
+let lastTime = 0;
+let deltaTime = 0;
+const update = (time = 0) => {
+    deltaTime = time - lastTime
+    lastTime = time
+
+
+    draw()
+    requestAnimationFrame(update)
+}
+
+solveBtn.addEventListener('click', () => {
+    solve(game.board)
+})
+
+canvas.addEventListener('click', event => {
+    let mousePos = getMousePos(event)
+    let indexes = getIndex(mousePos)
+    let [x, y] = indexes.getPos()
+    console.log(x, y)
+    game.cell = indexes
+})
+
+
+
+document.addEventListener('keydown', event => {
+    let input = parseInt(event.key)
+    let pos = game.cell
+    if (game.cell) {
+        console.log(event.key)
+        if (input || input === 0) {
+            console.log(game.board, game.cell)
+            game.board[pos.y][pos.x].value = input
+            if (isValidNumber(pos, game.board, game.board[pos.y][pos.x])) {
+                game.board[pos.y][pos.x].stat = 'success'
+            } else {
+                game.board[pos.y][pos.x].stat = 'error'
+            }
+        }
+    }
+})
+
+const setUpBoardObject = () => {
+    const b = game.originalBoard
+    const board = []
+    for (let y = 0; y < b.length; y++) {
+        const row = []
+        for (let x = 0; x < b.length; x++) {
+            const block = new Block(new Vector2(x, y), b[y][x], null)
+            row.push(block)
+        }
+        board.push(row)
+    }
+    game.board = board
+}
+setUpBoardObject()
+console.log(game.board)
+update()
 
