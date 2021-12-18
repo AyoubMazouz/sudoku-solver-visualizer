6a4f386d2fcfa7b2173a2ef03c9ecaeb35632246
= <=>
2021-12-19 00:13:34 +0100

    Refactor draw function & bug fixes

--- sudoku-solver.js
+++ sudoku-solver.js
@@ -54,17 +54,18 @@ const select = event => {
     if (game.cell) game.cell = null
     const [x, y] = calculateIndex([event.clientX, event.clientY])
     if (x > 9 || y > 9) return
-    if (game.board[y][x].canChange) game.cell = [x, y]
+    game.cell = [x, y]
 }
 
 const keyDown = event => {
     if (!game.cell) return
     if (isNaN(event.key)) return
+    const [x, y] = game.cell
+    if (!game.board[y][x].canChange) return
 
     const number = parseInt(event.key)
-    const [x, y] = game.cell
 
-    if (isValidNumber([x, y], game.originalBoard, number)) {
+    if (isValidNumber([x, y], convertBoard(game.board), number)) {
         game.board[y][x].stat = 'success'
     } else {
         game.board[y][x].stat = 'error'
@@ -72,26 +73,58 @@ const keyDown = event => {
     game.board[y][x].value = number
 }
 
-const drawSelection = (board, cell) => {
-    if (!cell) return
-    const [x, y] = cell
-    const [gridX, gridY] = [(x / 3 | 0) * 3, (y / 3 | 0) * 3]
-    ctx.fillStyle = 'rgba(0, 0, 255, .3)'
-    ctx.fillRect(gridX * cellSize, gridY * cellSize, cellSize * 3, cellSize * 3)
+const drawSameNumberSelection = (board, x, y) => {
     for (let i = 0; i < board.length; i++) {
-        ctx.fillRect(x * cellSize, i * cellSize, cellSize, cellSize)
-        ctx.fillRect(i * cellSize, y * cellSize, cellSize, cellSize)
+        for (let j = 0; j < board.length; j++) {
+            ctx.fillStyle = '#BBDEFB'
+            if (board[y][x].value === board[i][j].value && board[y][x].value !== 0) {
+                ctx.fillRect(j * cellSize, i * cellSize, cellSize, cellSize)
+            }
+        }
     }
 }
 
-const drawStaticElements = (board) => {
-    for (let y = 0; y < board.length; y += 1) {
-        for (let x = 0; x < board.length; x += 1) {
-            ctx.strokeStyle = 'rgba(0,0,0,.3)'
-            ctx.lineWidth = 1
-            ctx.strokeRect(ctx.lineWidth + x * cellSize, y * cellSize, cellSize, cellSize)
+const drawHorizontalVerticalSelection = (board, x, y) => {
+    for (let i = 0; i < board.length; i++) {
+        ctx.fillStyle = '#E2EBF3'
+        ctx.fillRect(x * cellSize, i * cellSize, cellSize, cellSize)
+        ctx.fillRect(i * cellSize, y * cellSize, cellSize, cellSize)
+        if (board[y][x].stat === 'error') {
+            ctx.fillStyle = '#F7CFD6'
+            if (board[y][x].value === board[y][i].value
+                && x !== i) {
+                ctx.fillRect(i * cellSize, y * cellSize, cellSize, cellSize)
+            }
+            if (board[y][x].value === board[i][x].value
+                && y !== i) {
+                ctx.fillRect(x * cellSize, i * cellSize, cellSize, cellSize)
+            }
         }
     }
+}
+
+const drawSubGridSelection = (board, x, y, gridX, gridY) => {
+    for (let i = gridY; i < gridY + 3; i++) {
+        for (let j = gridX; j < gridX + 3; j++) {
+            ctx.fillStyle = '#E2EBF3'
+            ctx.fillRect(j * cellSize, i * cellSize, cellSize, cellSize)
+            if (board[y][x].stat === 'error') {
+                ctx.fillStyle = '#F7CFD6'
+                if (board[y][x].value === board[i][j].value
+                    && [x, y] !== [j, i]) {
+                    ctx.fillRect(j * cellSize, i * cellSize, cellSize, cellSize)
+                }
+            }
+        }
+    }
+}
+
+const drawCellSelection = (x, y) => {
+    ctx.fillStyle = '#BBDEFB'
+    ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize)
+}
+
+const drawSubGridBorder = board => {
     for (let y = 0; y < board.length; y += 3) {
         for (let x = 0; x < board.length; x += 3) {
             ctx.strokeStyle = 'rgba(0,0,0,1)'
@@ -102,17 +135,28 @@ const drawStaticElements = (board) => {
 
 }
 
-const drawBoard = board => {
+const drawCellBorder = board => {
+    for (let y = 0; y < board.length; y++) {
+        for (let x = 0; x < board.length; x++) {
+            ctx.strokeStyle = 'rgba(0,0,0,.3)'
+            ctx.lineWidth = 1
+            ctx.strokeRect(ctx.lineWidth + x * cellSize, y * cellSize, cellSize, cellSize)
+        }
+    }
+}
+
+const drawNumbers = board => {
     ctx.font = `${cellSize}px sans serif`
     ctx.textAlign = 'center'
     ctx.textBaseline = 'middle'
+    ctx.fillStyle = '#000'
     for (let y = 0; y < board.length; y++) {
         for (let x = 0; x < board.length; x++) {
             const posX = (cellSize / 2) + (x * cellSize)
             const posY = (cellSize / 2) + (y * cellSize)
             const text = game.board[y][x].value > 0 ? game.board[y][x].value : ''
-            ctx.fillStyle = determineColor(game.board[y][x])
-            ctx.fillText(text, posX, posY)
+            ctx.fillStyle =
+                ctx.fillText(text, posX, posY)
         }
     }
 }
@@ -124,20 +168,31 @@ const game = {
     over: false
 }
 
-const draw = () => {
-    clearCanvas()
-    drawStaticElements(game.board)
-    drawSelection(game.board, game.cell)
-    drawBoard(game.board)
+const draw = game => {
+    const board = game.board
 
+    clearCanvas()
+    if (game.cell) {
+        const [x, y] = game.cell
+        const [gridX, gridY] = [(x / 3 | 0) * 3, (y / 3 | 0) * 3]
+        drawSameNumberSelection(board, x, y)
+        drawHorizontalVerticalSelection(board, x, y)
+        drawSubGridSelection(board, x, y, gridX, gridY)
+        drawCellSelection(x, y)
+    }
+    drawNumbers(board)
+    drawCellBorder(board)
+
+    drawSubGridBorder(board)
 }
+
 
 let lastTime = 0;
 let deltaTime = 0;
 const update = (time = 0) => {
     deltaTime = time - lastTime
     lastTime = time
-    draw()
+    draw(game)
     window.requestAnimationFrame(update)
 }
 
