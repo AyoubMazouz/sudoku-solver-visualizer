1f79ede5e34ea3a9a4a6de65aa8a5372184d6d7c
= <=>
2021-12-23 20:45:27 +0100

    Restyling using tailwindcss.

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      'montserrat': ['"montserrat"']
+    },
+    screen: {
+      'lg': '950px'
+    },
+    extend: {
+      colors: {
+        "primary": "#0072E3",
+        "secondary": "#45576E",
+        "btn-bg": "#e5edf5",
+        "btn-bg-hover": "#ccd6e2"
+      }
+    },
+  },
+  plugins: [],
+}
